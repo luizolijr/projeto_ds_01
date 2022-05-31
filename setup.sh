fadfa31cace060e7e74fc671a18609b151a57070
Luiz <luiz-dh@hotmail.com>
2022-05-31 17:37:27 -0300

    first

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/credentials.toml
+echo "\
+[general]\n\email = luiz-dh@hotmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableC0RS=false\n\
+port = $P0RT\n\
+" > ~/.streamlit/config.toml
