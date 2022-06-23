42d5268f4a2b6d38eecdde38ac22ad3cfd65242b
Luiz <luiz-dh@hotmail.com>
2022-06-22 23:42:12 -0300

    gz222

--- setup.sh
+++ setup.sh
@@ -1,11 +1,20 @@
-mkdir -p ~/.streamlit/
-echo "\
-[general]\n\email = luiz-dh@hotmail.com\"\n\
-" > ~/.streamlit/credentials.toml
+mkdir -p ~/.streamlit
 
-echo "\
-[server]\n\
-headless = true\n\
-enableC0RS=false\n\
-port = $P0RT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
+
+
+#mkdir -p ~/.streamlit/
+#echo "\
+#[general]\n\email = luiz-dh@hotmail.com\"\n\
+#" > ~/.streamlit/credentials.toml
+#
+#echo "\
+#[server]\n\
+#headless = true\n\
+#enableC0RS=false\n\
+#port = $P0RT\n\
+#" > ~/.streamlit/config.toml
